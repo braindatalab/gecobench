cd82ff137285fdd8c940172411625affe0eb4e25
rick <rickwg@mailbox.org>
2023-04-11 14:34:00 +0200

    Update training bash script

--- job_training.sh
+++ job_training.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
-#$ -binding linear:8
+#$ -binding linear:4
 #$ -N nlp-benchmark-training
 ##$ -l h_rt=24:00:00
 ##$ -l s_rt=24:00:00
 #$ -l h_vmem=20G
 #$ -l mem_free=20G
+#$ -l cuda=1
 ##$ -m abe
 ##$ -M rick.wilming@tu-berlin.de
 
@@ -13,4 +14,4 @@ export WORKDIR=/home/space/uniml/rick/$1
 cd $WORKDIR
 ls -l
 apptainer build --fakeroot --force /home/rick/nlp-apptainerfile-training.sif nlp-apptainerfile-training.def
-apptainer run --bind $DATADIR:/mnt --bind $WORKDIR:/workdir /home/rick/nlp-apptainerfile-training.sif
+apptainer run --nv --bind $DATADIR:/mnt --bind $WORKDIR:/workdir /home/rick/nlp-apptainerfile-training.sif
