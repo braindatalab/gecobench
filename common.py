821e5391e83857fd2c3947034ef599b741b4b1bd
rick <rickwg@mailbox.org>
2023-05-02 12:32:07 +0200

    Update common

--- common.py
+++ common.py
@@ -2,4 +2,5 @@ from collections import namedtuple
 
 NAME_OF_PROJECT_CONFIG = 'project_config.json'
 
+DataTargetPair = namedtuple('DataPair', 'data target')._make([list(), list()])
 DataSet = namedtuple('DataSet', 'x_train y_train x_test y_test')
