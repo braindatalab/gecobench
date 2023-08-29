9aa02a0494e544ac6a1955613400d32832f9c29c
rick <rickwg@mailbox.org>
2023-08-29 14:58:18 +0200

    Add clust job script for slurm

---
+++ cluster_job_slurm.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#SBATCH --job xai-nlp-benchmark
+#SBATCH --partition=gpu-2h
+#SBATCH --gpus-per-node=1
+#SBATCH --ntasks-per-node=2
+#SBATCH --output=logs/xai-nlp-benchmark-job-%j.out
+
+
+export DATADIR=/home/space/uniml/rick/data
+export WORKDIR=/home/space/uniml/rick/$1
+cd $WORKDIR
+ls -l
+apptainer build --fakeroot --force /home/rick/nlp-apptainerfile.sif nlp-apptainerfile.def
+apptainer run --nv --bind $DATADIR:/mnt --bind $WORKDIR:/workdir /home/rick/nlp-apptainerfile.sif "$2" "$3"
+#apptainer run --bind $DATADIR:/mnt --bind $WORKDIR:/workdir /home/rick/nlp-apptainerfile.sif "$2" "$3"
