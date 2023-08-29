1a394d29543724f9f260c5e5fafac47b4ae23fcb
rick <rickwg@mailbox.org>
2023-08-29 14:40:19 +0200

    Refactor file names

--- copy_data_to_cluster.sh
+++ copy_data_to_cluster.sh
@@ -8,8 +8,8 @@ scp artifacts/$1/data/training_all.pkl rick@hydra:/home/space/uniml/rick/data/ar
 scp artifacts/$1/data/training_subject.pkl rick@hydra:/home/space/uniml/rick/data/artifacts/$1/data/training_subject.pkl
 scp artifacts/$1/data/test_female_all.pkl rick@hydra:/home/space/uniml/rick/data/artifacts/$1/data/test_female_all.pkl
 scp artifacts/$1/data/test_female_subject.pkl rick@hydra:/home/space/uniml/rick/data/artifacts/$1/data/test_female_subject.pkl
+scp artifacts/$1/data/test_subject.pkl rick@hydra:/home/space/uniml/rick/data/artifacts/$1/data/test_subject.pkl
+scp artifacts/$1/data/test_all.pkl rick@hydra:/home/space/uniml/rick/data/artifacts/$1/data/test_all.pkl
 scp artifacts/$1/data/test_male_all.pkl rick@hydra:/home/space/uniml/rick/data/artifacts/$1/data/test_male_all.pkl
 scp artifacts/$1/data/test_male_subject.pkl rick@hydra:/home/space/uniml/rick/data/artifacts/$1/data/test_male_subject.pkl
-scp artifacts/$1/data/ground_truth_all.pkl rick@hydra:/home/space/uniml/rick/data/artifacts/$1/data/ground_truth_all.pkl
-scp artifacts/$1/data/ground_truth_subject.pkl rick@hydra:/home/space/uniml/rick/data/artifacts/$1/data/ground_truth_subject.pkl
 scp artifacts/$1/data/project_config.json rick@hydra:/home/space/uniml/rick/data/artifacts/$1/data/project_config.json
