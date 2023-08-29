5f37945acbf4079869d290105de47ec2aa8d849c
rick <rickwg@mailbox.org>
2023-08-29 14:13:02 +0200

    Update cluster name

--- copy_data_to_cluster.sh
+++ copy_data_to_cluster.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-ssh rick@cluster-ida "mkdir /home/space/uniml/rick/data/artifacts"
-ssh rick@cluster-ida "mkdir /home/space/uniml/rick/data/artifacts/$1"
-ssh rick@cluster-ida "mkdir /home/space/uniml/rick/data/artifacts/$1/data"
+ssh rick@hydra "mkdir /home/space/uniml/rick/data/artifacts"
+ssh rick@hydra "mkdir /home/space/uniml/rick/data/artifacts/$1"
+ssh rick@hydra "mkdir /home/space/uniml/rick/data/artifacts/$1/data"
 
-scp artifacts/$1/data/training_all.pkl rick@cluster-ida:/home/space/uniml/rick/data/artifacts/$1/data/training_all.pkl
-scp artifacts/$1/data/training_subject.pkl rick@cluster-ida:/home/space/uniml/rick/data/artifacts/$1/data/training_subject.pkl
-scp artifacts/$1/data/test_female_all.pkl rick@cluster-ida:/home/space/uniml/rick/data/artifacts/$1/data/test_female_all.pkl
-scp artifacts/$1/data/test_female_subject.pkl rick@cluster-ida:/home/space/uniml/rick/data/artifacts/$1/data/test_female_subject.pkl
-scp artifacts/$1/data/test_male_all.pkl rick@cluster-ida:/home/space/uniml/rick/data/artifacts/$1/data/test_male_all.pkl
-scp artifacts/$1/data/test_male_subject.pkl rick@cluster-ida:/home/space/uniml/rick/data/artifacts/$1/data/test_male_subject.pkl
-scp artifacts/$1/data/ground_truth_all.pkl rick@cluster-ida:/home/space/uniml/rick/data/artifacts/$1/data/ground_truth_all.pkl
-scp artifacts/$1/data/ground_truth_subject.pkl rick@cluster-ida:/home/space/uniml/rick/data/artifacts/$1/data/ground_truth_subject.pkl
-scp artifacts/$1/data/project_config.json rick@cluster-ida:/home/space/uniml/rick/data/artifacts/$1/data/project_config.json
+scp artifacts/$1/data/training_all.pkl rick@hydra:/home/space/uniml/rick/data/artifacts/$1/data/training_all.pkl
+scp artifacts/$1/data/training_subject.pkl rick@hydra:/home/space/uniml/rick/data/artifacts/$1/data/training_subject.pkl
+scp artifacts/$1/data/test_female_all.pkl rick@hydra:/home/space/uniml/rick/data/artifacts/$1/data/test_female_all.pkl
+scp artifacts/$1/data/test_female_subject.pkl rick@hydra:/home/space/uniml/rick/data/artifacts/$1/data/test_female_subject.pkl
+scp artifacts/$1/data/test_male_all.pkl rick@hydra:/home/space/uniml/rick/data/artifacts/$1/data/test_male_all.pkl
+scp artifacts/$1/data/test_male_subject.pkl rick@hydra:/home/space/uniml/rick/data/artifacts/$1/data/test_male_subject.pkl
+scp artifacts/$1/data/ground_truth_all.pkl rick@hydra:/home/space/uniml/rick/data/artifacts/$1/data/ground_truth_all.pkl
+scp artifacts/$1/data/ground_truth_subject.pkl rick@hydra:/home/space/uniml/rick/data/artifacts/$1/data/ground_truth_subject.pkl
+scp artifacts/$1/data/project_config.json rick@hydra:/home/space/uniml/rick/data/artifacts/$1/data/project_config.json
