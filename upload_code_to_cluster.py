c7b1823d19021b75fe1edfe65c629db6e7233edc
rick <rickwg@mailbox.org>
2023-09-01 17:05:20 +0200

    Update/refactoring such that the script works for hydra and hcp

--- upload_code_to_cluster.py
+++ upload_code_to_cluster.py
@@ -7,7 +7,18 @@ from os.path import basename, join
 from time import sleep
 from paramiko.client import SSHClient
 
-from utils import load_json_file, append_date
+from utils import append_date
+
+CLUSTER_NAMES = ['hydra', 'hpc']
+CLUSTER_DOMAINS = dict(
+    hydra='hydra.ml.tu-berlin.de',
+    hpc='gateway.hpc.tu-berlin.de'
+)
+
+CLUSTER_BASE_DIRS = dict(
+    hydra='/home/space/uniml/rick',
+    hpc='/home/users/r/rick'
+)
 
 
 def create_tarfile(source_dir: str, output_filename: str) -> None:
@@ -21,8 +32,13 @@ def create_tarfile(source_dir: str, output_filename: str) -> None:
         tar.add(name=source_dir, arcname=basename(source_dir), filter=filter_function)
 
 
-def copy_file_to_cluster(source_path: str, target_path: str, user_name: str = 'rick') -> None:
-    command = ['scp', source_path, f'{user_name}@hydra:{target_path}']
+def copy_file_to_cluster(
+        cluster_name: str,
+        source_path: str,
+        target_path: str,
+        user_name: str = 'rick'
+) -> None:
+    command = ['scp', source_path, f'{user_name}@{cluster_name}:{target_path}']
     _ = subprocess.run(command, stdout=subprocess.PIPE)
 
 
@@ -36,10 +52,10 @@ def pause_script(seconds: int):
     sleep(seconds)
 
 
-def get_ssh_connection_to_cluster() -> SSHClient:
+def get_ssh_connection_to_cluster(cluster_name: str) -> SSHClient:
     client = SSHClient()
     client.load_host_keys(filename='/home/rick/.ssh/known_hosts')
-    client.connect('hydra.ml.tu-berlin.de')
+    client.connect(CLUSTER_DOMAINS[cluster_name])
     return client
 
 
@@ -47,32 +63,55 @@ def extract_tar_file(ssh_client: SSHClient, file_path: str) -> str:
     file_path_split = file_path.split('/')
     target_dir = '/'.join(file_path_split[:-1])
     target_filename = file_path_split[-1].split('.')[0]
-    command = f'tar -xf {file_path} -C {target_dir} --one-top-level={target_filename} --strip-components 1'
-    ssh_client.exec_command(command=command)
+    target_path = f'{target_dir}/{target_filename}'
+    command_mkdir = f'mkdir {target_path}'
+    command_untar = f'tar -xf {file_path} -C {target_path} --strip-components 1'
+    ssh_client.exec_command(command=f'{command_mkdir} && {command_untar}')
     return join(target_dir, target_filename)
 
 
-def main() -> None:
-    config_cluster = load_json_file(file_path='cluster_config.json')
+def main(cluster_name: str) -> None:
     current_dir = os.getcwd()
-
     filename_compressed_code = f'{append_date(s=basename(current_dir))}.tar.gz'
     print('Create tar archive of source code.')
     create_tarfile(source_dir=current_dir, output_filename=filename_compressed_code)
 
     print('Copy code to cluster.')
-    target_path_compressed_code = join(config_cluster['base_dir'], filename_compressed_code)
-    copy_file_to_cluster(source_path=filename_compressed_code, target_path=target_path_compressed_code)
+    target_path_compressed_code = join(
+        CLUSTER_BASE_DIRS[cluster_name],
+        filename_compressed_code
+    )
+    copy_file_to_cluster(
+        cluster_name=cluster_name,
+        source_path=filename_compressed_code,
+        target_path=target_path_compressed_code
+    )
 
     print('Extract tar file on cluster.')
     pause_script(seconds=5)
-    client = get_ssh_connection_to_cluster()
-    target_dir = extract_tar_file(ssh_client=client, file_path=target_path_compressed_code)
+    _ = extract_tar_file(
+        ssh_client=get_ssh_connection_to_cluster(cluster_name=cluster_name),
+        file_path=target_path_compressed_code
+    )
 
     pause_script(seconds=5)
     remove_local_file(file_path=filename_compressed_code)
     pause_script(seconds=5)
 
 
+def get_command_line_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--cluster',
+        dest='cluster',
+        required=True,
+        help='Name of cluster',
+        type=str,
+        default=1,
+    )
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
-    main()
+    args = get_command_line_arguments()
+    main(cluster_name=args.cluster)
