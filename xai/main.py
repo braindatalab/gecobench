52db920c98c12ba00ee64c7d7cde6e45abbdd1a4
rick <rickwg@mailbox.org>
2023-11-06 10:56:44 +0100

    Add more verbose logging

--- xai/main.py
+++ xai/main.py
@@ -353,10 +353,12 @@ def main(config: Dict) -> None:
         training_records=training_records, data=test_data, config=config
     )
 
+    logger.info('Map raw attributions to original words.')
     results = map_raw_attributions_to_original_tokens(
         xai_results_paths=intermediate_results_paths, config=config
     )
 
+    logger.info('Dump path records.')
     output_dir = generate_xai_dir(config=config)
     dump_as_pickle(
         data=results, output_dir=output_dir, filename=config['xai']['xai_records']
