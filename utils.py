634e5fef8bef517e6b780b8665c9428206c108d3
rick <rickwg@mailbox.org>
2023-11-27 11:21:39 +0100

    Fix selection of root directory for data

--- utils.py
+++ utils.py
@@ -12,8 +12,8 @@ import torch
 from numpy.random import Generator
 from torch.utils.data import TensorDataset, random_split
 
-CLUSTER_PLATFORM_NAME = '#102-Ubuntu'
-CLUSTER_DATA_DIR = '/mnt'
+LOCAL_PLATFORM_NAME = '#37~22.04.1-Ubuntu'
+LOCAL_DIR = ''
 
 
 def load_pickle(file_path: str) -> Any:
@@ -43,16 +43,16 @@ def append_date(s: str) -> str:
     return f'{s}-{date}'
 
 
-def get_root_dir_based_on_platform() -> str:
-    return CLUSTER_DATA_DIR if CLUSTER_PLATFORM_NAME in platform.version() else ''
+def on_local_platform() -> bool:
+    return True if LOCAL_PLATFORM_NAME in platform.version() else False
 
 
 def generate_data_dir(config: Dict) -> str:
     return join(
-        get_root_dir_based_on_platform(),
+        LOCAL_DIR if on_local_platform() else config['general']['apptainer_data_dir'],
         config['general']['base_dir'],
         config['general']['data_scenario'],
-        config['data']['output_dir']
+        config['data']['output_dir'],
     )
 
 
@@ -60,7 +60,7 @@ def generate_training_dir(config: Dict) -> str:
     return join(
         config['general']['base_dir'],
         config['general']['data_scenario'],
-        config['training']['output_dir']
+        config['training']['output_dir'],
     )
 
 
@@ -68,7 +68,7 @@ def generate_xai_dir(config: Dict) -> str:
     return join(
         config['general']['base_dir'],
         config['general']['data_scenario'],
-        config['xai']['output_dir']
+        config['xai']['output_dir'],
     )
 
 
@@ -76,7 +76,7 @@ def generate_evaluation_dir(config: Dict) -> str:
     return join(
         config['general']['base_dir'],
         config['general']['data_scenario'],
-        config['evaluation']['output_dir']
+        config['evaluation']['output_dir'],
     )
 
 
@@ -84,7 +84,7 @@ def generate_visualization_dir(config: Dict) -> str:
     return join(
         config['general']['base_dir'],
         config['general']['data_scenario'],
-        config['visualization']['output_dir']
+        config['visualization']['output_dir'],
     )
 
 
@@ -99,7 +99,4 @@ def create_train_val_split(data: TensorDataset, val_size: float) -> List:
     num_samples = len(data)
     num_val_samples = int(val_size * num_samples)
     num_train_samples = num_samples - num_val_samples
-    return random_split(
-        dataset=data,
-        lengths=[num_train_samples, num_val_samples]
-    )
+    return random_split(dataset=data, lengths=[num_train_samples, num_val_samples])
