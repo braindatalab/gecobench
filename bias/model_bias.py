8233018ea0270ee553f826add813d014d81483cc
venondev <venondesign@gmail.com>
2024-05-27 11:58:40 +0200

    minor refactoring of loops and plot adjustment

--- bias/model_bias.py
+++ bias/model_bias.py
@@ -2,6 +2,7 @@ from itertools import chain
 from itertools import chain
 from os.path import join
 from typing import Dict, Any
+from time import time
 
 import numpy as np
 import pandas as pd
@@ -11,9 +12,10 @@ from loguru import logger
 from tqdm import tqdm
 from transformers import AutoModelForMaskedLM
 import matplotlib.pyplot as plt
+from matplotlib import rc
 import seaborn as sns
 
-from common import validate_dataset_key
+from common import validate_dataset_key, SaveVersion
 from training.bert import (
     create_bert_ids,
     get_bert_tokenizer,
@@ -32,6 +34,7 @@ from utils import (
     ONE_LAYER_ATTENTION_MODEL_TYPE,
     determine_model_type,
     generate_bias_dir,
+    generate_visualization_dir,
 )
 from visualization.common import (
     MODEL_NAME_MAP,
@@ -39,6 +42,7 @@ from visualization.common import (
     METRIC_NAME_MAP,
     MODEL_ORDER,
     ROW_ORDER,
+    GENDER,
 )
 
 DEVICE = 'cpu'
@@ -69,16 +73,12 @@ def create_bias_results(
 
 
 def calculate_model_bias_metrics_on_sentence(
-    model: Any,
+    generator_model: Any,
     row: pd.Series,
     dataset_type: str,
-    trained_on_dataset_name: str,
     model_params: dict,
     config: dict,
-    num_samples: int,
-    index: int,
 ) -> list[dict]:
-    logger.info(f'Dataset type: {dataset_type}, sentence: {index} of {num_samples}')
     model_type = determine_model_type(s=model_params['model_name'])
     tokenizer = get_tokenizer[model_type](config)
 
@@ -97,9 +97,9 @@ def calculate_model_bias_metrics_on_sentence(
     for sentence in sentence_batch:
         masked_sentences += [' '.join(sentence)]
 
-    token_ids = tokenizer(masked_sentences, return_tensors='pt', padding=True)
-    generator_model = AutoModelForMaskedLM.from_pretrained('bert-base-uncased')
-    generator_model.bert = model.bert
+    token_ids = tokenizer(masked_sentences, return_tensors='pt', padding=True).to(
+        DEVICE
+    )
     token_logits = generator_model(**token_ids).logits
     # Find the location of [MASK] and extract its logits
     mask_token_index = torch.where(token_ids['input_ids'] == tokenizer.mask_token_id)[1]
@@ -108,7 +108,9 @@ def calculate_model_bias_metrics_on_sentence(
     for i, k in enumerate(mask_token_index):
         # Pick the [MASK] candidates with the highest softmax
         max_softmax_mask_token_logit = torch.max(softmax_mask_token_logits[i, k, :])
-        mlm_score += (-1) * torch.log(max_softmax_mask_token_logit).detach().numpy()
+        mlm_score += (-1) * torch.log(
+            max_softmax_mask_token_logit
+        ).detach().cpu().numpy()
 
     results = create_bias_results(
         mlm_score=mlm_score,
@@ -128,21 +130,25 @@ def calculate_model_bias_metrics(
     model_params: dict,
     config: dict,
 ) -> list[dict]:
-    num_samples = dataset.shape[0]
+    generator_model = AutoModelForMaskedLM.from_pretrained('bert-base-uncased')
+    generator_model.bert = model.bert
+    generator_model = generator_model.to(DEVICE)
 
-    results = Parallel(n_jobs=1)(
-        delayed(calculate_model_bias_metrics_on_sentence)(
-            model,
-            row,
-            dataset_type,
-            trained_on_dataset_name,
-            model_params,
-            config,
-            num_samples,
-            k,
+    results = []
+    for _, row in tqdm(
+        dataset.iterrows(),
+        total=len(dataset),
+        desc=f"Processing sentences for {dataset_type} for model trained on {trained_on_dataset_name}.",
+    ):
+        results.append(
+            calculate_model_bias_metrics_on_sentence(
+                generator_model,
+                row,
+                dataset_type,
+                model_params,
+                config,
+            )
         )
-        for k, (_, row) in enumerate(dataset.iterrows())
-    )
 
     return results
 
@@ -166,7 +172,7 @@ def loop_over_training_records(
             logger.info(
                 f'Processing {model_params["model_name"]} trained on {trained_on_dataset_name} with dataset {dataset_name}.'
             )
-            dataset = pd.concat([data[dataset_name][:2], data[dataset_name][-2:]], axis=0)
+            dataset = data[dataset_name]
             model = load_model(path=join(artifacts_dir, model_path)).to(DEVICE)
 
             result = calculate_model_bias_metrics(
@@ -213,14 +219,21 @@ create_token_ids = {
 }
 
 
-def main(config: Dict) -> None:
-    artifacts_dir = generate_artifacts_dir(config=config)
+def create_results(config: Dict, artifacts_dir: str, output_dir: str):
     training_records_path = join(
         artifacts_dir,
         generate_training_dir(config=config),
         config['training']['training_records'],
     )
     training_records = load_pickle(file_path=training_records_path)
+    training_records = list(
+        filter(
+            lambda x: x[1]["save_version"] == SaveVersion.best
+            and x[1]['model_name'] != 'one_layer_attention_classification',
+            training_records,
+        )
+    )
+
     test_data = load_test_data(config=config)
     logger.info(f'Generate explanations.')
     intermediate_results_paths = loop_over_training_records(
@@ -228,40 +241,52 @@ def main(config: Dict) -> None:
     )
 
     logger.info('Dump path records.')
-    output_dir = join(artifacts_dir, generate_bias_dir(config=config))
     dump_as_pickle(
         data=intermediate_results_paths,
         output_dir=output_dir,
         filename='bias_records.pkl',
     )
 
+
+def visualize_results(config: Dict, artifacts_dir: str, output_dir: str):
+    logger.info('Visualize model bias results.')
     data_paths = load_pickle(file_path=join(output_dir, 'bias_records.pkl'))
     list_of_dicts = list()
     for p in data_paths:
         d = load_pickle(file_path=join(artifacts_dir, p))
         list_of_dicts += d
 
+    logger.info('Loaded data.')
+
     data = pd.DataFrame(list_of_dicts)
 
     data['mapped_model_name'] = data['model_name'].map(lambda x: MODEL_NAME_MAP[x])
     data['dataset_type'] = data['dataset_type'].map(lambda x: DATASET_NAME_MAP[x])
+    data['target'] = data['target'].map(lambda x: GENDER[x])
 
     data = data.rename(
         columns={
             "mapped_model_name": "Model",
             "dataset_type": "Dataset",
+            "mlm_score": "MLM Score",
+            "target": "Class",
         }
     )
 
+    logger.info('Creating plot')
     g = sns.catplot(
         data=data,
         x='Model',
-        y='mlm_score',
-        order=MODEL_ORDER,
-        # hue_order='target',
-        # row_order=ROW_ORDER,
-        hue='target',
-        row='Dataset',
+        y='MLM Score',
+        order=list(
+            filter(
+                lambda x: x != MODEL_NAME_MAP["one_layer_attention_classification"],
+                MODEL_ORDER,
+            )
+        ),
+        col_order=ROW_ORDER,
+        hue='Class',
+        col='Dataset',
         kind='box',
         palette=sns.color_palette(palette='pastel'),
         fill=True,
@@ -269,10 +294,43 @@ def main(config: Dict) -> None:
         fliersize=0,
         estimator='median',
         # aspect=9.5 / height,
-        legend_out=True,
+        legend_out=False,
+        legend=True,
     )
 
-    plt.show()
+    sns.move_legend(g, 'upper center', bbox_to_anchor=(0.935, 0.95), ncol=1)
+
+    g.set_titles(
+        row_template='Dataset: {row_name}', col_template='Dataset: {col_name}', size=12
+    )
+    for ax in g.axes.flatten():
+        ax.set_xlabel('')
+        ax.set_ylabel('MLM Score', fontsize=12)
+
+        for tick in ax.get_xticklabels():
+            tick.set_fontsize(12)
+
+    # Set legend font size
+    g._legend.set_title('Class', prop={'size': 12})
+    for t in g._legend.texts:
+        t.set_fontsize(12)
+
+    visualization_dir = generate_visualization_dir(config)
+    plt.savefig(
+        join(visualization_dir, 'model_bias.png'),
+        dpi=300,
+        bbox_inches='tight',
+    )
+    plt.close()
+
+
+def main(config: Dict) -> None:
+    rc('text', usetex=True)
+    artifacts_dir = generate_artifacts_dir(config=config)
+    output_dir = join(artifacts_dir, generate_bias_dir(config=config))
+
+    create_results(config=config, artifacts_dir=artifacts_dir, output_dir=output_dir)
+    visualize_results(config=config, artifacts_dir=artifacts_dir, output_dir=output_dir)
 
 
 if __name__ == '__main__':
